29a40c3868ab70c9fb21caf73e8879e60500aa18
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2026-06-28 09:39:35 -0300

    Criando o componente Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veniam enim
-        a quos sed harum ipsum alias deleniti hic, minus quia eveniet delectus
-        saepe placeat modi neque quis? Ipsa, distinctio.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
