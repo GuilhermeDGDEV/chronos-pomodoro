0328ccaf6845a157bd37e5032f057cbaffd3bd13
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2026-08-05 06:44:05 -0300

    Atualizando o contador

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -4,6 +4,8 @@ export enum TaskActionTypes {
   START_TASK = 'START_TASK',
   INTERRUPT_TASK = 'INTERRUPT_TASK',
   RESET_STATE = 'RESET_STATE',
+  COUNT_DOWN = 'COUNT_DOWN',
+  COMPLETE_TASK = 'COMPLETE_TASK',
 }
 
 export type TaskActionsWithPayload =
@@ -12,12 +14,20 @@ export type TaskActionsWithPayload =
       payload: TaskModel;
     }
   | {
-      type: TaskActionTypes.INTERRUPT_TASK;
+      type: TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
     };
 
-export type TaskActionsWithoutPayload = {
-  type: TaskActionTypes.RESET_STATE;
-};
+export type TaskActionsWithoutPayload =
+  | {
+      type: TaskActionTypes.RESET_STATE;
+    }
+  | {
+      type: TaskActionTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: TaskActionTypes.COMPLETE_TASK;
+    };
 
 export type TaskActionModel =
   | TaskActionsWithPayload
